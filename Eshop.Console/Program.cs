ea713b9bb5b5dc43f7ad85a56cb796d05f19119a
akonaweb <akonaweb.sk@gmail.com>
2024-03-03 14:50:15 +0100

    Initial Domain project with Product class

--- Eshop.Console/Program.cs
+++ Eshop.Console/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Eshop.Domain;
+
+var products = new List<Product>
+{
+    new Product("Notebook Acer 15", "Best notebook out there", 399.99m),
+    new Product("Mouse Razor 123", "Best Mouse", 14.50m),
+    new Product("Title", "Description", 123)
+};
+
+foreach (var product in products)
+{
+    Console.WriteLine($"{product.Title} | {product.Description} | {product.Price}");
+}
