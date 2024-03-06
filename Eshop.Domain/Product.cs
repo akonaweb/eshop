5868e9dd1d12df98e0b54bb1557aec3db488ee0e
akonaweb <akonaweb.sk@gmail.com>
2024-03-06 20:15:17 +0100

    Web api project + Controller for Product

--- Eshop.Domain/Product.cs
+++ Eshop.Domain/Product.cs
@@ -2,14 +2,43 @@
 {
     public class Product
     {
-        public Product(string title, string description, decimal price)
+        public Product(int id, string title, string description, decimal price)
         {
-            if (string.IsNullOrEmpty(title))
+            if (id < 0)
+            {
+                throw new ArgumentNullException("id");
+            }
+
+            ValidateParameters(title, description, price);
+
+            Id = id;
+            Title = title;
+            Description = description;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+
+        public void Update(string title, string description, decimal price)
+        {
+            ValidateParameters(title, description, price);
+
+            Title = title;
+            Description = description;
+            Price = price;
+        }
+
+        private static void ValidateParameters(string title, string description, decimal price)
+        {
+            if (string.IsNullOrEmpty(title) || title.Length > 50)
             {
                 throw new ArgumentNullException("title");
             }
 
-            if (string.IsNullOrEmpty(description))
+            if (string.IsNullOrEmpty(description) || description.Length > 500)
             {
                 throw new ArgumentNullException("description");
             }
@@ -18,14 +47,6 @@
             {
                 throw new ArgumentNullException("price");
             }
-
-            Title = title;
-            Description = description;
-            Price = price;
         }
-
-        public string Title { get; }
-        public string Description { get; }
-        public decimal Price { get; }
     }
 }
---
+++ Eshop.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Eshop.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product(1, "Notebook Acer 15", "Best notebook out there", 399.99m),
+            new Product(2, "Mouse Razor 123", "Best Mouse", 14.50m),
+        };
+
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            return Products;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            return Products.First(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public Product CreateProduct(string title, string description, decimal price)
+        {
+            var newId = Products.Max(x => x.Id) + 1;
+            var newProduct = new Product(newId, title, description, price);
+            
+            Products.Add(newProduct);
+
+            return newProduct;
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteProduct(int id)
+        {
+            var productToBeDeleted = Products.First(x => x.Id == id);
+            Products.Remove(productToBeDeleted);
+        }
+
+        [HttpPut("{id}")]
+        public Product UpdateProduct(int id, string title, string description, decimal price)
+        {
+            var productToBeUpdated = Products.First(x => x.Id == id);
+            productToBeUpdated.Update(title, description, price);
+
+            return productToBeUpdated;
+        }
+    }
+}
