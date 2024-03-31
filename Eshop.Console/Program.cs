c0f578dced2353ed498d0f9d729b843cd0a7253d
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2024-03-31 20:50:03 +0200

    connected Categories to Products in Products added new parameter Category In webAPI added as well

--- Eshop.Console/Program.cs
+++ Eshop.Console/Program.cs
@@ -1,15 +1,15 @@
 ﻿using Eshop.Domain;
 
-var products = new List<Product>
-{
-    new Product(1, "Notebook Acer 15", "Best notebook out there", 399.99m),
-    new Product(2, "Mouse Razor 123", "Best Mouse", 14.50m),
-};
-
 var categories = new List<Category>
 {
     new Category(1, "Computers"),
     new Category(2, "Mouses")
+};
+
+var products = new List<Product>
+{
+    new Product(1, "Notebook Acer 15", "Best notebook out there", 399.99m, categories[0]),
+    new Product(2, "Mouse Razor 123", "Best Mouse", 14.50m, categories[1])
 };
 
 foreach (var category in categories)
@@ -19,5 +19,5 @@ foreach (var category in categories)
 
 foreach (var product in products)
 {
-    Console.WriteLine($"{product.Title} | {product.Description} | {product.Price}");
+    Console.WriteLine($"{product.Title} | {product.Description} | {product.Price} | {product.Category.Name}");
 }
--- Eshop.Domain/Product.cs
+++ Eshop.Domain/Product.cs
@@ -2,36 +2,39 @@
 {
     public class Product
     {
-        public Product(int id, string title, string description, decimal price)
+        public Product(int id, string title, string description, decimal price, Category category)
         {
             if (id < 0)
             {
                 throw new ArgumentOutOfRangeException("id");
             }
 
-            ValidateParameters(title, description, price);
+            ValidateParameters(title, description, price, category);
 
             Id = id;
             Title = title;
             Description = description;
             Price = price;
+            Category = category;
         }
 
-        public int Id { get; private set; }
+        public int Id { get; }
         public string Title { get; private set; }
         public string Description { get; private set; }
         public decimal Price { get; private set; }
+        public Category Category { get; private set; }
 
-        public void Update(string title, string description, decimal price)
+        public void Update(string title, string description, decimal price, Category category)
         {
-            ValidateParameters(title, description, price);
+            ValidateParameters(title, description, price, category);
 
             Title = title;
             Description = description;
             Price = price;
+            Category = category;
         }
 
-        private static void ValidateParameters(string title, string description, decimal price)
+        private static void ValidateParameters(string title, string description, decimal price, Category category)
         {
             if (string.IsNullOrEmpty(title) || title.Length > 50)
             {
@@ -46,6 +49,11 @@
             if (price < 0)
             {
                 throw new ArgumentNullException("price");
+            }
+
+            if (category == null) 
+            {
+                throw new ArgumentNullException("category");    
             }
         }
     }
