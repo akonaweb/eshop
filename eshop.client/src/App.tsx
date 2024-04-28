2f759bc06271497dac32d2c52d1161b71a8b9f82
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2024-04-28 21:20:39 +0200

    added connection with products read descrpition!
    this is not-working commit of connected products in react but I have to add it to my branch because I can work on it while I'll be in school from my another computer

--- eshop.client/src/App.tsx
+++ eshop.client/src/App.tsx
@@ -5,20 +5,42 @@ type CategoryType = {
   name: string
 }
 
+type ProductType = {
+  id: number
+  title: string
+  description: string
+  price: number
+}
+
+
+
 function App() {
   const [categories, setCategories] = useState<CategoryType[]>([])
   useEffect(()=>{
     const loadCategories = async() =>{
-      const result = await (await fetch('https://localhost:7203/Category')).json()
+      const categoryResult = await (await fetch('https://localhost:7203/Category')).json()
 
-      setCategories(result)
+      setCategories(categoryResult)
     }
 
     loadCategories();
   }, [])
 
+  const [products, setProducts] = useState<ProductType[]>([])
+  useEffect(()=>{
+    const loadProducts = async() =>{
+      const productResult = await (await fetch('https://localhost:7203/Category')).json()
+
+      setProducts(productResult)
+    }
+
+    loadProducts();
+  }, [])
+
+
+
   return (
-    <ul>
+    <div>
       {categories.map((x) => {
         return (
           <li key={x.id}>
@@ -26,7 +48,16 @@ function App() {
           </li>
       )
       })}
-    </ul>
+
+      {products.map((x) => {
+        return (
+          <li key={x.id}>
+            {x.id} - {x.title} - {x.description} -{x.price}
+          </li>
+      )
+      })}
+    </div>
+
   );
 
  
